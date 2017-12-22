fce7889268c5f23611ec70cb22994d690ab07a3a
ichinator <lazonegeek1@gmail.com>
2017-12-22 16:58:26 +0100

    add News entity ( no form yet ), add navbar to fos layout and add accueil and news link to easyadmin  layout

---
+++ src/AppBundle/Entity/News.php
@@ -0,0 +1,206 @@
+<?php
+// src/AppBundle/Entity/News.php
+
+namespace AppBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="news")
+ */
+
+class News
+{
+  /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+  private $id;
+
+  /**
+     * @ORM\Column(type="datetime")
+     */
+  private $date;
+
+  /**
+     * @ORM\Column(type="string", length=100)
+     */
+  private $title;
+
+  /**
+     * @ORM\Column(type="string", length=100)
+     */
+  private $author;
+
+  /**
+     * @ORM\Column(type="text")
+     */
+  private $content;
+
+  /**
+     * @ORM\Column(type="boolean")
+     */
+  private $published = true;
+
+  /**
+     * @ORM\Column(type="string", length=100)
+     */
+  private $categories;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return News
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return News
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     *
+     * @return News
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return News
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set published
+     *
+     * @param boolean $published
+     *
+     * @return News
+     */
+    public function setPublished($published)
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    /**
+     * Get published
+     *
+     * @return boolean
+     */
+    public function getPublished()
+    {
+        return $this->published;
+    }
+
+    /**
+     * Set categories
+     *
+     * @param string $categories
+     *
+     * @return News
+     */
+    public function setCategories($categories)
+    {
+        $this->categories = $categories;
+
+        return $this;
+    }
+
+    /**
+     * Get categories
+     *
+     * @return string
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+}
